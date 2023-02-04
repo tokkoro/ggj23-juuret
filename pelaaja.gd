5bab086b32e49823ed7a5fc9d4356c9fe7a94b7f
VesaVesa <87971172+VesaVesa@users.noreply.github.com>
2023-02-04 21:10:21 +0200

    yo i fixed ur shit
    no problem mates

--- pelaaja.gd
+++ pelaaja.gd
@@ -1,14 +1,12 @@
 extends RigidBody2D
 
 var player_number = 0
-<<<<<<< Updated upstream
 var dead = 0
 
-=======
 var x_velocity = 0.0
 var x_last_tick = 0.0
 var im_hit_no_collision_timer = Timer.new();
->>>>>>> Stashed changes
+
 func _ready():
 	print("p", player_number)
 

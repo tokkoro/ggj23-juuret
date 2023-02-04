8c0a3beb8dd5a78d5e7067baf6169bb124750038
oppilas <oppilas@htkmtav-41416.local>
2023-02-04 20:23:15 +0200

    ripen potaattis

--- Grenade/Grenade.gd
+++ Grenade/Grenade.gd
@@ -2,18 +2,13 @@ extends RigidBody2D
 
 var from_player = null
 var spawn_time = 0
+var preripened = false
 
 func _ready():
-	print("test")
 	contact_monitor = true
 	contacts_reported = 5
 	connect("body_entered", self, "_body_entered_asdf")
 	spawn_time = Time.get_ticks_msec()
-	print("potaattis")
-
-func _process(delta):
-	print(get_parent())
-	print(global_position)
 
 func _body_entered_asdf(node: PhysicsBody2D):
 	var is_ground = node.get_collision_mask_bit(10)
@@ -22,6 +17,7 @@ func _body_entered_asdf(node: PhysicsBody2D):
 		# istutu
 		var sprout = preload("res://Sprout/potato_sprout.tscn").instance()
 		sprout.position = position
+		sprout.ripe = preripened
 		var sprout_owner = get_node("../sprout_owner")
 		sprout_owner.add_child(sprout)
 		sprout_owner.sprouts.append(sprout)

97ef7df143f64d6a1050dab0c829292614c844b9
Raattis <Raattis@users.noreply.github.com>
2023-02-04 17:40:20 +0200

    Player input handling.

--- maailma.gd
+++ maailma.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var round_end_curtain_effect = $curtains.material
 
+onready var input = $input
+
 signal round_start
 signal round_end
 signal transition_halfway
@@ -11,7 +13,7 @@ var transition = false
 var transition_halfway = false
 
 const TRANSITION_DURATION = 5.0
-const ROUND_DURATION = 10.0
+const ROUND_DURATION = 3.0
 
 var players = []
 var player_spawn_points = [Vector2(0,-100), Vector2(100,-100), Vector2(300,-100), Vector2(400,-100)]
@@ -39,20 +41,35 @@ func new_round():
 func end_round():
 	# play round end audio
 	transition = true
+	transition_halfway = false
 	print("signal: round_end")
 	emit_signal("round_end")
 
 func _ready():
 	new_round()
 
+func _physics_process(delta):
+	for player_number in range(len(players)):
+		var left = Input.is_action_pressed(input.get_input_string("player_left_", player_number))
+		var right = Input.is_action_pressed(input.get_input_string("player_right_", player_number))
+		var throw = Input.is_action_pressed(input.get_input_string("player_throw_", player_number))
+		var x = 0.0
+		if left:
+			x += -1.0
+		if right:
+			x += 1.0
+		players[player_number].get_node("GrenadeThrow").throw_held = throw
+		players[player_number].add_central_force(Vector2(x * 100.0, 0))
+
 func _process(delta):
 	round_time_left -= delta
 	if transition:
 		if not transition_halfway && round_time_left < -TRANSITION_DURATION * 0.5:
 			transition_halfway = true
-			for player in players:
-				player.queue_free()
-			players = []
+			for i in range(len(players)):
+				player_spawn_points[i].x = players[i].position.x
+				players[i].queue_free()
+			players.clear()
 			print("singal: transition_halfway")
 			emit_signal("transition_halfway")
 		

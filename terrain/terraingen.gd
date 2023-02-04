a80faf9c9c9be9c0d693fd1639246988dfd64e9b
VesaVesa <87971172+VesaVesa@users.noreply.github.com>
2023-02-04 16:31:21 +0200

    Revert "Merge branch 'master' of https://github.com/tokkoro/ggj23-juuret"
    This reverts commit db1827bd30e3bba92ab9cef78aef4573d1fe5238, reversing
    changes made to 26a0dff90ddf8c02d9d0da8faeb7b8f9fb0ad7c7.

--- terrain/terraingen.gd
+++ terrain/terraingen.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var ground_far_color: Color = Color.brown.linear_interpolate(Color.black, 0.75);
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,54 +9,90 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	genterrain(750, 100);
+	randomize();
+	genterrain(750, 100, 100, rand_range(0, 1e16));
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func genterrain(width: float, height: float):
+class GroundPoint:
+	var p: Vector2
+	var color: Color
+
+
+func genterrain(width: float, height: float, offset: float, seed_v: float):
 	print("Generate terrain!");
 	
-	var verts = PoolVector2Array()
-	
 	var sc: float = 100;
-	var wave_h: float = 50;
-	var division_count = 100;
+	var division_count = 128;
 	
-	var ground_points = [];
+	var wave_h: float = 40;
+	var wave_f_arr = [];
+	var rand_h = 0;
+	var rand_gen = RandomNumberGenerator.new()
+	rand_gen.seed = seed_v;
+	for wave_i in range(0, 8):
+		wave_f_arr.push_back(rand_gen.randf() * 20);
 	
+	var ground_points = [];	
 	for i in range(0, division_count):
 		var i_f = float(i);
 		var t: float = i_f / division_count;
 		var x = t * width;
 		
-		ground_points.push_back(Vector2(
-			t * width,
-			sin(t * PI * 6) * wave_h
-		));
+		var gp = GroundPoint.new();
 		
+		var h = 0;
+		for wave_f in wave_f_arr:
+			h += sin(wave_f * PI * t) * wave_h / wave_f_arr.size()
+			
+		gp.p = Vector2(
+			t * width,
+			h + offset
+		);
+		
+		gp.color = Color.brown.linear_interpolate(Color.black, rand_range(0, 0.3));
+		
+		ground_points.push_back(gp);
+	
+	var verts = PoolVector2Array()
+	var colors = PoolColorArray()
 	for i in range(1, ground_points.size() - 1):
+		var i_a = i - 1;
+		var i_b = i;
+		
+		var gp_a = ground_points[i - 1];
+		var gp_b = ground_points[i];
+		
 		# a---b
 		# | \ |
 		# d---c
-		var p_a = ground_points[i - 1];
-		var p_b = ground_points[i];
-		var p_c = Vector2(p_b.x, height);
-		var p_d = Vector2(p_a.x, height);
+		var p_a = gp_a.p;
+		var p_b = gp_b.p;
+		var p_c = Vector2(p_b.x, offset + height);
+		var p_d = Vector2(p_a.x, offset + height);
 		
 		verts.push_back(p_a);
+		colors.push_back(gp_a.color);
 		verts.push_back(p_b);
+		colors.push_back(gp_b.color);
 		verts.push_back(p_c);
+		colors.push_back(ground_far_color);
 		
 		verts.push_back(p_a);
+		colors.push_back(gp_a.color);
 		verts.push_back(p_c);
+		colors.push_back(ground_far_color);
 		verts.push_back(p_d);
+		colors.push_back(ground_far_color);
+		
 	
 	# Initialize the ArrayMesh.
 	var arrs = []
 	arrs.resize(ArrayMesh.ARRAY_MAX)
 	arrs[ArrayMesh.ARRAY_VERTEX] = verts
+	arrs[ArrayMesh.ARRAY_COLOR] = colors
 	
 	var arr_mesh = ArrayMesh.new()
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrs)
@@ -63,7 +100,9 @@ func genterrain(width: float, height: float):
 	get_node("terrain_mesh").mesh = arr_mesh
 	
 	var col = PoolVector2Array();
-	col.append_array(ground_points);
-	col.append(Vector2(ground_points.back().x, height));
-	col.append(Vector2(ground_points.front().x, height));
-	get_node("StaticBody2D/CollisionPolygon2D").polygon = col;
+	for gp in ground_points:
+		col.append(gp.p);
+	col.append(Vector2(ground_points.back().p.x, offset + height));
+	col.append(Vector2(ground_points.front().p.x, offset + height));
+	get_node("terrain_body/poly").polygon = col;
+	

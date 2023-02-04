902b02b10b05bca1373f96cdb5b3c6c6c68123e5
VesaVesa <87971172+VesaVesa@users.noreply.github.com>
2023-02-04 18:19:23 +0200

    Update maailma.gd
    added aim direction

--- maailma.gd
+++ maailma.gd
@@ -60,6 +60,10 @@ func _physics_process(delta):
 			x += 1.0
 		players[player_number].get_node("GrenadeThrow").throw_held = throw
 		players[player_number].apply_central_impulse(Vector2(x * 10.0, 0))
+		if left:
+			players[player_number].get_node("Sprite").flip_h = true
+		elif right:
+			players[player_number].get_node("Sprite").flip_h = false
 
 func _process(delta):
 	round_time_left -= delta
